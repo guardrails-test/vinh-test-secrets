e71f65925ce6ea2c2470952d09290ef13a0d1b66
guardrails-test <63639880+guardrails-test@users.noreply.github.com>
2023-08-02 16:46:35 +0700

    Update test.js

--- test.js
+++ test.js
@@ -11,3 +11,6 @@ var POLYGON_API_KEY = "u6QCF1YPrgfUokLgcT9QctNIYmZ49rL4";
 var ABYSSALE_API_KEY = "3YpLVEQLnz2HhhJiz3uLra5soUNeut556N5lEsKB"
 var SHOPIFY_DOMAIN = "https://lmfaohahaxd.myshopify.com/something/lol"
 var SHOPIFY_TOKEN = "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"
+
+var MAILGUN_KEY = "1a3f7b2349d8933bbd6b9c6e44e59f1a-73f745ed-fbd786b4"
+var MAILGUN_SECRET = "key-b6c04df17812937716f10033f005ee0c"
